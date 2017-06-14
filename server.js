72db0581d26f54ed25a0b5c19c166fd0ff8a90d7
Ayas87 <randylam87@gmail.com>
2017-06-13 21:05:09 -0700

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-const express = require('express');
-const url = require('url');
+const express = require("express");
 const path = require('path');
-const body = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
+const port = process.env.port || 3000;
 
-app.set('port', 3000);
-app.use(express.static(path.join(__dirname, 'public')));
-const server = app.listen(app.get('port'), function() {
-  const port = server.address().port;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+app.get('/:key?',(req,res)=>{
+    let key = req.params.key;
+    switch(key){
+        case "survey":
+            res.sendFile(path.join(`${__dirname}/public/`, "survey.html"));
+        break;
+        default:
+            res.sendFile(path.join(`${__dirname}/public/`, "home.html"));
+    }
 });
 
+
+app.listen(port,(req,res)=>{
+    console.log(`Server is listening on port ${port}`);
+});
