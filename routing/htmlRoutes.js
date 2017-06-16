4d5f41877c10ab6dae7819f737a63a13a0d0222d
Ayas87 <randylam87@gmail.com>
2017-06-15 18:47:35 -0700

    routing is kind of working

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,2 +1,16 @@
 // A GET Route to /survey which should display the survey page.
 // A default USE route that leads to home.html which displays the home page.
+
+const path = require('path');
+const routes = {
+    surveyHtml: (app) => {
+        app.get('/survey', (req, res) => {
+            res.sendFile(path.join(__dirname, '../public', 'survey.html'));
+        });
+    },
+    homeHtml: (app,express) => {
+        console.log(path.join(__dirname, '../public'));
+        app.use(express.static(path.join(__dirname, '../public')));
+    }
+};
+module.exports = routes;
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const routes = require('./routing/htmlRoutes.js');
 
-
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -13,19 +13,27 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({
   type: "application/vnd.api+json"
 }));
+//routing
+routes.homeHtml(app, express);
 
-app.get('/:key?',(req,res)=>{
-    let key = req.params.key;
-    switch(key){
-        case "survey":
-            res.sendFile(path.join(`${__dirname}/public/`, "survey.html"));
-        break;
-        default:
-            res.sendFile(path.join(`${__dirname}/public/`, "home.html"));
-    }
-});
+routes.surveyHtml(app);
 
+//survey + home
+// app.get('/:key?', (req, res) => {
+// let key = req.params.key;
+// switch (key) {
+//     case "survey":
+//         res.sendFile(path.join(`${__dirname}/public/`, "survey.html"));
+//         break;
+//     default:
+//         res.sendFile(path.join(`${__dirname}/public/`, "home.html"));
+// }
+// });
 
 app.listen(PORT,(req,res)=>{
     console.log(`Server is listening on PORT ${PORT}`);
+});
+
+app.post('/survey',(req,res)=>{
+    console.log(req.body);
 });
